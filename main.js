67af6ad94fa981b1bb0ca3a2941abd89f3429a91
Jean <154983968+JeanAlves00@users.noreply.github.com>
2025-02-04 13:39:10 -0400

    1.0

--- main.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('navbar-toggle').addEventListener('click', function() {
-    var menu = document.getElementById('navbar-menu');
-    if (menu.style.display === 'flex') {
-        menu.style.display = 'none';
-    } else {
-        menu.style.display = 'flex';
-    }
-});
